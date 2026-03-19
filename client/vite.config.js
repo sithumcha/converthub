724ead4e2e2843a42b64a0d7dcdc3addeb8f0f61
User <sithumchanukasandaruwan2002@gmail.com>
2026-03-19 23:01:35 +0530

    modified:   client/vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // warning limit එක 1MB දක්වා වැඩි කරන්න
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['react-hot-toast'],
+        }
+      }
+    }
+  }
 })
