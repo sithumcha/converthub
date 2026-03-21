56ae32d9ac3648246db57d8203357b0fd101fcf4
User <sithumchanukasandaruwan2002@gmail.com>
2026-03-21 15:44:54 +0530

    Fix: Allow all CORS origins for mobile access

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,3 +1,32 @@
+// const cors = require('cors');
+
+// const allowedOrigins = [
+//   'http://localhost:5173',
+//   'http://localhost:3000',
+//   'https://converthub-2026.netlify.app',
+//   'https://converthub-six.vercel.app'
+// ];
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       console.log('Blocked CORS request from:', origin);
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+//   exposedHeaders: ['Content-Disposition']
+// };
+
+// module.exports = cors(corsOptions);
+
+
 const cors = require('cors');
 
 const allowedOrigins = [
@@ -9,12 +38,14 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl)
     if (!origin) return callback(null, true);
+
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       console.log('Blocked CORS request from:', origin);
-      callback(new Error('Not allowed by CORS'));
+      callback(null, true);  // ✅ Allow anyway for testing
     }
   },
   credentials: true,
